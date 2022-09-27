c9d27b97a044fb8b854dda2786f1916e54136596
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-09-26 22:40:59 -0400

    added html and css

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var weatherAPIKey = af971021a3754fa8a1492437e6b37e01;
+var cityNameRequest = (https:"//api.openweathermap.org/data/2.5/weather?q={city name}&appid={af971021a3754fa8a1492437e6b37e01}")
+
+var city;
+
+
+
+
+
+
+
+
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={af971021a3754fa8a1492437e6b37e01}
+
+fetch (
+
+
+
+
