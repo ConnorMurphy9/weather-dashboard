67d490e767c6f6af938e259ee4e771fffa359b17
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-09-26 23:50:09 -0400

    added variables, started fetch function

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,21 @@
-var weatherAPIKey = af971021a3754fa8a1492437e6b37e01;
-var cityNameRequest = (https:"//api.openweathermap.org/data/2.5/weather?q={city name}&appid={af971021a3754fa8a1492437e6b37e01}")
-
-var city;
-
-
-
-
-
-
-
-
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={af971021a3754fa8a1492437e6b37e01}
-
-fetch (
+var APIKey = "af971021a3754fa8a1492437e6b37e01";
+var cityNameRequest = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={af971021a3754fa8a1492437e6b37e01}';
+var searchBtn = document.getElementById("searchBtn");
+addEventListener('click' , searchBtn);
+var city = document.getElementById("citySearch");
+console.log(city);
+var cityQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
 
 
+
+// function getApi(cityQueryURL) {
+//     fetch(cityQueryURL)
+//     .then(function (response) {
+//         console.log(response);
+//     }
+
+
+
+// }
